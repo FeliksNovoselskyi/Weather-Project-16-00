a745c43b25ff05f06f26fcda71b42a9137b40e06
WorldIT-academy <worldit_academy@worldit.academy>
2026-04-19 17:45:23 +0300

    Add utils

--- modules/weather_container.py
+++ modules/weather_container.py
@@ -1,5 +1,7 @@
 import PyQt6.QtCore as core
 import PyQt6.QtWidgets as widgets
+from utils import request
+import json
 
 class WeatherContainer(widgets.QFrame):
     def __init__(self, parent):
@@ -16,10 +18,20 @@ class WeatherContainer(widgets.QFrame):
         self.TOP_FRAME.setStyleSheet("background-color: grey")
         self.WEATHER_CONTEINER_LAYOUT.addWidget(self.TOP_FRAME)
         
+        response = request("Odesa")
+        print(json.dumps(response, indent = 4))
+        
+        
         self.CETRAL_FRAME = widgets.QFrame(self)
         self.CETRAL_FRAME.setFixedSize(788, 303)
         self.CETRAL_FRAME.setStyleSheet("background-color: grey")
         self.WEATHER_CONTEINER_LAYOUT.addWidget(self.CETRAL_FRAME)
+        
+        self.CENTRAL_LAYOUT = widgets.QHBoxLayout(self.CETRAL_FRAME)
+        self.CETRAL_FRAME.setLayout(self.CENTRAL_LAYOUT)
+
+        self.LABEL = widgets.QLabel(self.CETRAL_FRAME,text = str(response["main"]["temp"]))
+        self.CENTRAL_LAYOUT.addWidget(self.LABEL)
         
         self.FOOTER = widgets.QFrame(parent = self)
         self.FOOTER.setFixedSize(788, 364)
